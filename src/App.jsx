75dbb047f604a465954417938ed6d933fdbaf5db
Sayeeda Heena <heenheena28@gmail.com>
2025-12-16 11:32:47 +0400

    fix double router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter as Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Navbar from './component/Narbar'
 
