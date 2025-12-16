d27ea91128b95f9157186ecf6dffc7f09ef3ee3e
Sayeeda Heena <heenheena28@gmail.com>
2025-12-16 12:19:36 +0400

    add

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from './component/About';
 import Skills from './component/Skills';
 import Projects from './component/Projects';
 import Contact from './component/Contact';
+import Hero from './component/Hero';
 
 
 
