1def3a8e3510b8084101b8fc5ef8ec265b80ed47
Sayeeda Heena <heenheena28@gmail.com>
2025-12-16 13:24:21 +0400

    Remove mistake

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,12 @@
 import React from 'react'
 import Hero from '../component/Hero';
-import About from '../component/About';
-import Skills from '../component/Skills';
-import Projects from '../component/Projects';
-import Contact  from '../component/Contact';
-import Footer from '../component/Footer';
-
-
-
-
 
 const Home = () => {
   return (
     <div>
       <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
       
+
       
       
       
