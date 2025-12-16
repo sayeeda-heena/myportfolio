ccac0c8bbd29070e9c0da0c9397639876507f94f
Sayeeda Heena <heenheena28@gmail.com>
2025-12-16 12:34:40 +0400

    fix

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -48,4 +48,4 @@ const Hero = () => {
   )
 }
 
-export default Hero
+export default Hero;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,14 @@
 import React from 'react'
-import Hero from '../component/Hero'
-import About from '../component/About'
-import Skills from '../component/Skills'
-import Projects from '../component/Projects'
-import Contact from '../component/Contact'
-import Footer from '../component/Footer'
+import Hero from '../component/Hero';
+
 
 const Home = () => {
   return (
     <div>
       <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
+      
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
